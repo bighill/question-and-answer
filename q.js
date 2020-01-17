5e3dbb467c625c2cdb641a0e0d8c2f5611d3ce87
Alexander Bighill <alex@bighill.org>
2020-01-16 23:24:58 -0800

    Refactor2020 (#4)
    * refactor 2020

--- q.js
+++ q.js
@@ -1,93 +1,75 @@
-var fadeTime = 400;
+const questionPage = document.getElementById("question");
+const questionForm = document.getElementById("question-form");
+const questionInput = document.getElementById("question-input");
+const questionSubmit = document.getElementById("question-submit");
 
-$('#content')
-	.delay( 500 )
-	.fadeIn( fadeTime, function()
-		{
-			reset();
-		});
+const answerPage = document.getElementById("answer");
+const answerMessage = document.getElementById("answer-message");
+const answerReset = document.getElementById("reset");
 
-function opacity_1( el )
-{
-	el.animate({
-		opacity: 1
-	}, fadeTime );
-}
+var answerList = Array(
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful"
+);
 
-function opacity_0( el )
-{
-	el.animate({
-		opacity: 0
-	}, fadeTime );
-}
+const handleQuestionKeyed = ev => {
+  const val = ev.target.value;
+  if (val.length > 3) {
+    questionSubmit.classList.remove("invisible");
+  } else {
+    questionSubmit.classList.add("invisible");
+  }
+};
 
-function reset()
-{
-	opacity_1( $('h1') );
-	$('#q').fadeIn( fadeTime );
-	$('#q input').val('').focus();
-	$('#q-sub').css( 'opacity', 0 );
-	$('#answer').hide().css( 'height', $('#q').outerHeight() );
-	$('#timer').width(0);
-}
+const handleQuestionSubmit = ev => {
+  ev.preventDefault();
+  // get answer
+  const rand = Math.floor(Math.random() * answerList.length);
+  const answer = answerList[rand];
+  answerMessage.innerHTML = answer;
+  // change view
+  questionPage.classList.add("hide");
+  answerPage.classList.remove("hide");
+};
 
-var answers = Array(
-	"It is certain",
-	"It is decidedly so",
-	"Without a doubt",
-	"Yes definitely",
-	"You may rely on it",
-	"As I see it, yes",
-	"Most likely",
-	"Outlook good",
-	"Yes",
-	"Signs point to yes",
-	"Reply hazy try again",
-	"Ask again later",
-	"Better not tell you now",
-	"Cannot predict now",
-	"Concentrate and ask again",
-	"Don't count on it",
-	"My reply is no",
-	"My sources say no",
-	"Outlook not so good",
-	"Very doubtful"
-	);
+const handleReset = ev => {
+  if (ev) ev.preventDefault();
+  // clear
+  questionInput.value = "";
+  answerMessage.innerHTML = "";
+  // change view
+  questionPage.classList.remove("hide");
+  answerPage.classList.add("hide");
+  questionInput.focus();
+};
 
-function return_answer()
-{
-	$('h1').css( 'opacity', 0 );
-	$('#q-sub').css( 'opacity', 0 );
-	$('#q').hide();
+const listeners = () => {
+  questionInput.addEventListener("keyup", handleQuestionKeyed);
+  questionForm.addEventListener("submit", handleQuestionSubmit);
+  answerReset.addEventListener("click", handleReset);
+};
 
-	var answer = answers[Math.floor(Math.random()*answers.length)];
-
-	$('#a-text').text( answer );
-	$('#answer').fadeIn( fadeTime*3 );
-
-	$('#timer')
-		.animate({
-			width: $('#content').width()
-		},
-		5000,
-		function()
-		{
-			reset();			
-		});	
-}
-
-$('#q input').keyup( function()
-{	
-	if ( $(this).val().length > 2 )
-		//opacity_1( $('#q-sub') );
-		$('#q-sub').css( 'opacity', 1 );
-	else
-		//opacity_0( $('#q-sub') );
-		$('#q-sub').css( 'opacity', 0 );
-});
-
-$('form').on('submit', function(e)
-{
-	return_answer();
-	e.preventDefault();
-});
+const init = function() {
+  handleReset();
+  listeners();
+  document.removeEventListener("DOMContentLoaded", init);
+};
+document.addEventListener("DOMContentLoaded", init);
